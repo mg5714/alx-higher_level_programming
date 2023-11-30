0df14d7f2c42daf3ce25c56ae545cad6d2fa113f
mg5714 <mistergddora2000@gmail.com>
2023-11-30 17:01:08 +0200

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("0 argument.")
     else:
         print("{} argument:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
