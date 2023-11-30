c032f9cb1caea40a4f6ceafbf2b5db5fc4adfd79
mg5714 <mistergddora2000@gmail.com>
2023-11-30 15:40:42 +0200

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,8 @@
 
 from add_0 import add
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     a = 1
     b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    res = a + b
+print("{} + {} = {}".format(a, b, res))
