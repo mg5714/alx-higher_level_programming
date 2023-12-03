2ebb8ed4a44a163adf17bc0c1fa819343003802c
mg5714 <mistergddora2000@gmail.com>
2023-12-03 15:29:41 +0200

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if len(sentence) > 0:
-        return (len(sentence), sentence[0])
+        return len(sentence), sentence[0]
     else:
         return None, None
