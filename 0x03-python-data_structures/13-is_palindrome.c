3b8af54946850e89bdfe634556a82e9468c32e8f
mg5714 <mistergddora2000@gmail.com>
2023-12-04 23:56:48 +0200

    task14

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,26 @@
 #include "lists.h"
+#include <stddef.h>
+
+/**
+ * palind - Checks if the remaining list is a palindrome.
+ * @start: Pointer to the head of the remaining list.
+ * @end: Tail of the remaining list.
+ *
+ * Return: 1 if palindrome, 0 if not palindrome.
+ */
+int palind(listint_t **start, listint_t *end)
+{
+  if (end == NULL) {
+    return 1;
+  }
+
+  if (palind(start, end->next) && (*start)->n == end->n) {
+    *start = (*start)->next;
+    return 1;
+  }
+
+  return 0;
+}
 
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome.
@@ -8,25 +30,10 @@
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (palind(head, *head));
+  if (*head == NULL || *head == NULL) {
+    return 1;
+  }
+
+  return palind(head, *head);
 }
 
-/**
- * palind -if is palindrome with recursion
- * @start: ptr to ptr
- * @end: pointer
- * Return: 1 palind,or 0 if not palind
- */
-int palind(listint_t **start, listint_t *end)
-{
-	if (end == NULL)
-		return (1);
-	if (palind(head, end->next) && (*head)->n == end->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
-}
