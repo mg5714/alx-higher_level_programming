c283d7368740b50cde0f9e65b21c8e97d5f430e0
mg5714 <mistergddora2000@gmail.com>
2023-12-06 16:15:19 +0200

    task7 8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best = {v: k for k, v in a_dictionary.items()}
+    best = {v: k for k, v in a_dictionary.items()} if a_dictionary else None
     return max(best)
