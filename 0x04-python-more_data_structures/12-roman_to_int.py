98bc9d026d480bda3972116516a4ab61876528eb
mg5714 <mistergddora2000@gmail.com>
2023-12-06 18:17:26 +0200

    task11

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    dig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    result = 0
+
+    for i in range(len(roman_string) - 1, -1, -1):
+        if i < len(roman_string) - 1 and dig[roman_string[i]] < dig[roman_string[i + 1]]:
+            result -= dig[roman_string[i]]
+        else:
+            result += dig[roman_string[i]]
+
+    return result
