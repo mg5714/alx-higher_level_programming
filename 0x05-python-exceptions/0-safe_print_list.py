4247e94131cc214f09d37618d298b4f3d9c3d1b3
mg5714 <mistergddora2000@gmail.com>
2023-12-18 18:41:47 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[count], end=" ")
             count += 1
         except IndexError:
-            pass
+            None
         print()
         return count
