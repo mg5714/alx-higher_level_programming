47e43744182510ed7fdc7e4ea58bc6676bb5670d
mg5714 <mistergddora2000@gmail.com>
2023-12-19 16:20:07 +0200

    task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ def a square """
+
+    def __init__(self, size):
+        """ create new obj of Square
+
+        args:
+            size: length of square
+        """
+        self._size = size
