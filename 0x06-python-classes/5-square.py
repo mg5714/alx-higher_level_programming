245383be1ebc54ce5fd47d98c0d9a18190b0131e
mofathi <Mohfabdou@gmail.com>
2023-12-20 01:13:17 +0200

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -43,11 +43,9 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        """ prints in stdout the square with the char # """
+        """ prints in stderr the square with the character # """
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
-                print()
-            for j in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            for k in range(self.__size):
+                print("#" * (self.__size))
