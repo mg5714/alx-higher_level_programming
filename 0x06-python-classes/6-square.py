f4a2cb308567f0b4047cf551e56dd733a23b0439
mg5714 <mistergddora2000@gmail.com>
2023-12-19 18:59:26 +0200

    task 3

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ def a square """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        initialize a square obj with a given size.
+
+        Args:
+            size: side length of square
+            position: position of the square as a tuple of 2 positive                       integers
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """
+        property for side of square.
+
+        Raises:
+            TypeError: if size not int.
+            ValueError: if size < 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """
+        return position of square
+
+        Raises:
+            TypeError: if position not tuple of 2 positive integers
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if vlaue[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        return self.__position = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            The current square area (integer).
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            space = " " * self.__position[1]
+            for i in range(self.__size):
+                print(f"{space}#{'#' * self.__size}")
