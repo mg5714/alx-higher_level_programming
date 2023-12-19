cce792a685c157d68f95c95cb30a843310733bfe
mg5714 <mistergddora2000@gmail.com>
2023-12-19 16:32:46 +0200

    task 0

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ def a square """
+
+
+    def __init__(self, size=0):
+        """
+        initialize a square obj with a given size.
+
+        Args:
+            size: side length of square
+
+        Raises:
+            TypeError: if size not int.
+            ValueError: if size < 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
