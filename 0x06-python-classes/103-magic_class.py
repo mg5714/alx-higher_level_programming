45919105bba56355f93bd0052df2644bbb93b127
mofathi <Mohfabdou@gmail.com>
2023-12-20 02:07:15 +0200

    task 5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -22,10 +22,10 @@ class MagicClass:
         """
         return the area of magic circle
         """
-        return math.pi * self.__radius ** 2
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """
         return circumference of magic circle
         """
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
