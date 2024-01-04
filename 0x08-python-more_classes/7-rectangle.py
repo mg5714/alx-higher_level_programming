f6d79d43595d39449d6e5213b65a93e5a33304ef
mofathi <Mohfabdou@gmail.com>
2024-01-04 20:18:16 +0200

    do

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -86,7 +86,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return "\n".join("#" * self.__width for _ in range(self.__height))
+            return "\n".join("#" * self.__width for i in range(self.__height))
 
     def __repr__(self):
         """Return a representation of the rectangle for recreation using eval()
