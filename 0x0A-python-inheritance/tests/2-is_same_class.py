17f96d43467b75365a2bfa3736a1572e60a1f55c
mg5714 <mistergddora2000@gmail.com>
2024-01-08 15:48:31 +0200

    1

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define module"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance
+
+    Args:
+        obj: An object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj is an instance of a_class, False otherwise.
+    """
+
+    return type(obj) is a_class
