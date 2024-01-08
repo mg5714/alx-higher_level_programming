d09131a1fc41bb76a24820e2a159b4dca1c676a9
mg5714 <mistergddora2000@gmail.com>
2024-01-08 19:20:18 +0200

    1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,6 +16,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Class Rectangle inherits from BaseGeometry."""
 
