f503439e2e081c46a8d646ad8a54807948f6970a
mg5714 <mistergddora2000@gmail.com>
2024-01-09 18:57:20 +0200

    task 11 12

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    try:
+        lines = []
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                line.append(line)
+                if search_string in line:
+                    lines.append(new_string)
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.writelines(lines)
+    except Exception as e:
+        pass
