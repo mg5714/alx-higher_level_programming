6889f3c364ceee55c3bfaedcb2184ce1da437a46
mg5714 <mistergddora2000@gmail.com>
2024-01-09 15:31:55 +0200

    task 0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define function"""
+
+
+def write_file(filename="", text=""):
+    """write text file"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return f.write(nothing to say)
