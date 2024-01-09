8509ab77c1c262f37e051e16fa3544ad98ce7725
mg5714 <mistergddora2000@gmail.com>
2024-01-09 19:02:53 +0200

    task 11 12

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,8 +6,8 @@ def append_after(filename="", search_string="", new_string=""):
     """append new string, search string in file"""
 
     try:
-        lines = []
         with open(filename, 'r', encoding='utf-8') as file:
+            lines = []
             for line in file:
                 line.append(line)
                 if search_string in line:
