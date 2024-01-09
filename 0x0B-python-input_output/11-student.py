047a7aa9c3f00d5087f78c694db1301743779fe8
mg5714 <mistergddora2000@gmail.com>
2024-01-09 17:10:32 +0200

    task 11 12

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""define studant class"""
+
+
+class Student:
+    """represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of Student"""
+        if attrs is None or not all(isinstance(attr, str) for attr in attrs):
+            return self.__dict__
+
+        return {attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
