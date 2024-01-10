9723975f7c955d91b4ba4ae1d905bbb10a062666
mg5714 <mistergddora2000@gmail.com>
2024-01-10 15:15:06 +0200

    t

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,24 @@
 """define script"""
 
 import sys
-import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+if __name__ == "__main__":
 
-arguments = sys.argv[1:]
+   try:
 
-existing_items = load_from_json_file("add_item.json")
-all_items = list(set(existing_items + arguments))
-save_to_json_file(all_items, "add_item.json")
-print("Arguments saved to add_item.json")
+       json_list = load_from_json_file('add_item.json')
+
+   except FileNotFoundError:
+
+       json_list = []
+
+
+   for i in range(1, len(sys.argv)):
+
+       json_list.append(sys.argv[i])
+
+   save_to_json_file(json_list, "add_item.json")
