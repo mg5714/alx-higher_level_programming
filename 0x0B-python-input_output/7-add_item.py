791ec50345caa27e4e1467dc04d89fd9f040ef60
mg5714 <mistergddora2000@gmail.com>
2024-01-10 15:05:53 +0200

    t

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except Exception:
-    my_list = []
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except Exception:
+        my_list = []
 
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
-save_to_json_file(my_list, "add_item.json")
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
