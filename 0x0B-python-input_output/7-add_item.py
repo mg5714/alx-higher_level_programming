6255e4c0b18a284384cdfb5951c08b5113d346bb
mg5714 <mistergddora2000@gmail.com>
2024-01-10 15:07:23 +0200

    t

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""define function"""
-
-
 import sys
 import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 if __name__ == "__main__":
     try:
-        my_list = load_from_json_file("add_item.json")
-    except Exception:
-        my_list = []
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
 
     for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, "add_item.json")
+        json_list.append(sys.argv[i])
+    save_to_json_file(json_list, "add_item.json")
