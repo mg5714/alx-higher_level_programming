32ecc93238ee4a1255bb4504b5c4150db342cde4
mg5714 <mistergddora2000@gmail.com>
2024-01-10 15:12:55 +0200

    t

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""define script"""
+
 import sys
 import json
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    try:
-        json_list = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        json_list = []
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-    for i in range(1, len(sys.argv)):
-        json_list.append(sys.argv[i])
-    save_to_json_file(json_list, "add_item.json")
+arguments = sys.argv[1:]
+
+existing_items = load_from_json_file("add_item.json")
+all_items = list(set(existing_items + arguments))
+save_to_json_file(all_items, "add_item.json")
+print("Arguments saved to add_item.json")
