1303c8418ee6398b96bca084e47ff30778b93450
mg5714 <mistergddora2000@gmail.com>
2024-01-10 15:15:51 +0200

    t

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-if __name__ == "__main__":
-
    try:
 
        json_list = load_from_json_file('add_item.json')
