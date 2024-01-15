61e62eda40b3aff17f059364e4eef77e9863875b
mg5714 <mistergddora2000@gmail.com>
2024-01-15 20:56:43 +0200

    go

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,9 +32,9 @@ class Base:
     def save_to_file(cls, list_objs):
         """Saves list of instances to json"""
         if list_objs is not None:
-            list_objs = [obj.to_dictionary() for obj in list_objs]
+            list_1 = [obj.to_dictionary() for obj in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as file:
-            file.write(cls.to_json_string(list_objs))
+            file.write(cls.to_json_string(list_1))
 
     @staticmethod
     def from_json_string(json_string):
