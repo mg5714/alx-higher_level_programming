c4410c63843b5e750aa8a4bb9fe16ff3e00f680e
mg5714 <mistergddora2000@gmail.com>
2024-01-15 20:00:42 +0200

    go

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a new Square instance"""
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Returns a string of square"""
