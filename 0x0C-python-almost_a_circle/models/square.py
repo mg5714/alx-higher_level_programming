ab60841ebf1f44cc1389a05062e63512d4ea5170
mg5714 <mistergddora2000@gmail.com>
2024-01-15 20:25:08 +0200

    go

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """Module for square class"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Represents a square, inherits from Rectangle."""
 
@@ -18,7 +19,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string representation of the square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
