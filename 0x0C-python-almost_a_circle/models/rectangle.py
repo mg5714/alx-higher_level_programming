0a6c4efe5fa3fdd56de119908945a476ba98619b
mg5714 <mistergddora2000@gmail.com>
2024-01-15 19:26:16 +0200

    go

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,23 +105,3 @@ class Rectangle(Base):
         """Returns a string representation of the rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".\
             format(self.id, self.x, self.y, self.width, self.height)
-
-    def updateed(self, id=None, width=None, height=None, x=None, y=None):
-        """update attributes"""
-        if id is not None:
-            self.id = id
-        if width is not None:
-            self.width = width
-        if height is not None:
-            self.height = height
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
-    def update(self, *args, **kwargs):
-        """Updates attributes of the rectangle."""
-        if args:
-            self.updateed(*args)
-        elif kwargs:
-            self.updateed(**kwargs)
