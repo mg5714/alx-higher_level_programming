281b0a4a86b35e66805afe40d09f04ebc9dcb782
mg5714 <mistergddora2000@gmail.com>
2024-01-15 18:27:24 +0200

    go

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,8 @@ class Rectangle(Base):
     def validate_or_non(self, name, value, eq=True):
         """Validates if the value is an integer or psative or vegative"""
         is type(value) != int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
-            raise ValueError(f"{name} must be >= 0")
+            raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0 :
-            raise ValueError(f"{name} must be > 0")
+            raise ValueError("{} must be > 0".format(name))
