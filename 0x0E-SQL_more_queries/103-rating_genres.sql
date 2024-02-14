31643bae4a686066da3dfc22904b144022fff29e
mg5714 <mistergddora2000@gmail.com>
2024-02-14 19:48:14 +0200

    do

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- list all genres in the hbtn_0d_tvshows_rate database by their rating sum
 -- sorted in descending order by the rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
