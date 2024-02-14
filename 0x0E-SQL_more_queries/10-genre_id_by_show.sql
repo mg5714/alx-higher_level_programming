677f25a0f6640cd7f4484f280ca8571a36c43bb0
mg5714 <mistergddora2000@gmail.com>
2024-02-14 18:25:14 +0200

    do

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- list all show in db.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_show_genres
+LEFT JOIN tv_shows ON tv_show_genres.id = tv_shows.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
