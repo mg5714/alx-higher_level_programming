d85798e922fb07313ea3748ef2a608c36470af77
mg5714 <mistergddora2000@gmail.com>
2024-02-14 19:43:53 +0200

    do

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,4 +5,4 @@ WHERE title NOT IN (
 	JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = "Comedy" )
-ORDER BY title ASC;
+ORDER BY 1 ASC;
