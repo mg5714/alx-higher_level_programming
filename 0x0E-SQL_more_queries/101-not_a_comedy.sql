b37998ed5503165af3ccd6e0d72d4b04964d2233
mg5714 <mistergddora2000@gmail.com>
2024-02-14 19:23:13 +0200

    do

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,8 +2,8 @@
 SELECT tv_shows.title
 FROM tv_shows
 WHERE tv_shows.id NOT IN (
-    SELECT tv_show_genres.show_id
-    FROM tv_show_genres
+    SELECT tv_shows.id
+    FROM tv_shows
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
