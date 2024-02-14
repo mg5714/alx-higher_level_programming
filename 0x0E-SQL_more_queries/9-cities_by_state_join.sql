f1d4f1f3a010ed0127439839674acdc3cb29b105
mg5714 <mistergddora2000@gmail.com>
2024-02-14 18:47:19 +0200

    do

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 --script connects h0d_usa database and executes query to retrieve all cities
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.state_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
