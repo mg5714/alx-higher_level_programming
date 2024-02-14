655c025c682f78859f46c4fb6e19a9db7d149077
mg5714 <mistergddora2000@gmail.com>
2024-02-14 19:35:52 +0200

    do

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,5 @@
 -- list all shows from the hbtn_0d_tvshows_rate database by their rating sum
-SELECT
-tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
