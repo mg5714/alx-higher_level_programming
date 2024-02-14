ebf5b2576789f11d30d3c522f357f3063197ad17
mg5714 <mistergddora2000@gmail.com>
2024-02-14 18:59:16 +0200

    do

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows from the hbtn_0d_tvshows_rate database by their rating sum
+SELECT tv_shows.title, SUM(rating) AS rating_sum
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- list all genres in the hbtn_0d_tvshows_rate database by their rating sum
+-- sorted in descending order by the rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
