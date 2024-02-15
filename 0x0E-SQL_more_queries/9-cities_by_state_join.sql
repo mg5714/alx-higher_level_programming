99a3e20efdd2f9ab2325217f55562ea8e0ec2ede
mofathi <Mohfabdou@gmail.com>
2024-02-15 12:39:37 +0200

    done

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 --script connects h0d_usa database and executes query to retrieve all cities.
---Each record should display: cities.id - cities.name - states.name
-SELECT cities.id, cities.name, states.name FROM cities
-JOIN states ON cities.state_id=states.id;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id;
