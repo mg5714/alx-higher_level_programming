2001ef65b2f087261d0aa988bc4d7367578b5cc0
mg5714 <mistergddora2000@gmail.com>
2024-03-11 18:55:24 +0200

    try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// script that searches the second biggest integer in the list of arguments.
+// script searches second biggest integer in list of arguments.
 
 if (process.argv.length <= 3) {
   console.log(0);
