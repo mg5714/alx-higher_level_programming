a144237ba5dfd502d8ff691abec906c31eb9bf6a
mg5714 <mistergddora2000@gmail.com>
2024-03-11 18:56:28 +0200

    try

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+// script Print string “Javascript is amazing”
 
+const myVar = 'Javascript is amazing';
+console.log(myVar);
