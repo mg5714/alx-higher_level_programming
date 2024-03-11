a8abec4f678046e94fbc00ec02a84f3a87a59c87
mg5714 <mistergddora2000@gmail.com>
2024-03-11 21:08:44 +0200

    try

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,11 +2,11 @@
 // script prints 3 lines but by using array of string and loop
 
 const lines = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
+  console.log(lines[i]);
 }
