de350d38204ab1c2d400473ab4481859fd017a82
mg5714 <mistergddora2000@gmail.com>
2024-03-12 16:30:31 +0200

    try

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
       return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
