733eb6760e1959133aa64f15df5886fc8957bf11
mg5714 <mistergddora2000@gmail.com>
2024-03-16 21:44:58 +0200

    try

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("""SELECT cities.name FROM cities INNER JOIN states ON
-            cities.state_id = states.id WHERE states.name = %s""", (sn,))
+            cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id""", (sn,))
 
     cities = cursor.fetchall()
 
