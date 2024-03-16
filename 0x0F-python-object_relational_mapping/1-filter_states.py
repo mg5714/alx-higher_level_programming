0656703e39365195b83e2bc0e92f21bb52b98f16
mg5714 <mistergddora2000@gmail.com>
2024-03-16 17:04:19 +0200

    try

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with 'N' (uppercase)
+from the database hbtn_0e_0_usa.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Database connection parameters
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Database connection parameters
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
