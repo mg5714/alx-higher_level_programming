66161c67e97b24c1f2c0de147923869729a6bb37
mg5714 <mistergddora2000@gmail.com>
2024-03-16 20:54:15 +0200

    try

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,16 @@ import sqlalchemy
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    host = "localhost"
+    port = 3306
+
+    db = MySQLdb.connect(
+            host=host, port=port, user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", [argv[4]])
+    cur.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY id ASC", [argv[4]])
 
     rows = cur.fetchall()
 
