7178a43c1f8d270fa4236cb4f801fccbd86a284e
mg5714 <mistergddora2000@gmail.com>
2024-03-28 04:17:38 +0200

    bye

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Send a request to the URL passed as argument and display only the status code
 curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Send a request to 0.0.0.0:5000/catch_me with curl
 curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L -H "Origin: You got me!"
