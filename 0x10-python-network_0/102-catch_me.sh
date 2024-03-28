28deb370f5051629f50b71fc9f3910a72bb06f33
mg5714 <mistergddora2000@gmail.com>
2024-03-28 04:23:08 +0200

    bye

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to 0.0.0.0:5000/catch_me with curl
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin: You got me!"
