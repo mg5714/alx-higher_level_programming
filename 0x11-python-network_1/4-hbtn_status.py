a358c56a410cda35f6c7d14b5748d98b74134e70
mg5714 <mistergddora2000@gmail.com>
2024-03-29 03:49:02 +0200

    go

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,10 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
-    data = response.json()
-
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
