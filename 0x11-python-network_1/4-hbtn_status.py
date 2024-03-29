63522047b104702d78effc286aeb2cf6afceebac
mg5714 <mistergddora2000@gmail.com>
2024-03-29 03:54:54 +0200

    go

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,10 @@
 """ script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
+
 if __name__ == "__main__":
-    res = requests.get("https://intranet.hbtn.io/status")
+    """fetches https://intranet.hbtn.io/status using requests"""
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
