49e04345fd55b7cd2f3e67add41aef7e3b8f7f5e
mg5714 <mistergddora2000@gmail.com>
2024-03-29 03:19:17 +0200

    go

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+
 if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
