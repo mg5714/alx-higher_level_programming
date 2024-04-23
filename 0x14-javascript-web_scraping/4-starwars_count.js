38a030411a41fd10bba8d9b0993d6222be180c6e
mg5714 <mistergddora2000@gmail.com>
2024-04-24 01:30:30 +0200

    bye

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,17 +4,13 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = '18'; // ID for Wedge Antilles
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const filmsWithWedge = filmsData.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(filmsWithWedge.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,19 +5,4 @@
 const fs = require('fs');
 const request = require('request');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Content has been saved to ${filePath}`);
-      }
-    });
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
